3cb3250dea08e40f2fd02c711a280ad8b686fa78
krolbot <krolbot@zaitsv.dev>
2026-08-27 02:06:26 +0300

    release: 0.1.20

--- build.gradle.kts
+++ build.gradle.kts
@@ -66,8 +66,8 @@ subprojects {
             minSdk = 23
             targetSdk = 35
 
-            versionName = "0.1.18"
-            versionCode = 11899
+            versionName = "0.1.20"
+            versionCode = 12099
 
             resValue("string", "release_name", "v$versionName")
             resValue("integer", "release_code", "$versionCode")
